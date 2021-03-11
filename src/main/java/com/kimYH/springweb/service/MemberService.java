8005bcee13c9eea95a3046401dc622031c10b490
이성웅 <dev.lsw91@gmail.com>
2021-03-12 01:06:35 +0900

    <AOP가 필요한 상황> MemberService 메소드 2개에 시간측정 로직 추가(일일히 추가해야됨)

--- src/main/java/com/kimYH/springweb/service/MemberService.java
+++ src/main/java/com/kimYH/springweb/service/MemberService.java
@@ -5,7 +5,6 @@ import com.kimYH.springweb.repository.MemberRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Transactional
 public class MemberService {
@@ -20,9 +19,16 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -36,10 +42,13 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
-    }
-
-    public Optional<Member> findOne(Long memberId) {
-        return memberRepository.findById(memberId);
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 }
